f46ac2129139a8b19eabbfe667d8355ee5d2f2e0
pavelkatunin <wellcome>
2022-01-22 00:23:56 +0300

    not working nested json

--- lib/data/api/model/api_task_list.dart
+++ lib/data/api/model/api_task_list.dart
@@ -11,6 +11,39 @@ class ApiTaskList {
       //   notfinished = map['notfinished'];
 
 }
+@JsonSerializable()
+class Person {
+  /// The generated code assumes these values exist in JSON.
+  final String firstName, lastName;
+
+  /// The generated code below handles if the corresponding JSON value doesn't
+  /// exist or is empty.
+  final DateTime? dateOfBirth;
+
+  Person({required this.firstName, required this.lastName, this.dateOfBirth});
+
+  /// Connect the generated [_$PersonFromJson] function to the `fromJson`
+  /// factory.
+  factory Person.fromJson(Map<String, dynamic> json) => _$PersonFromJson(json);
+
+  /// Connect the generated [_$PersonToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$PersonToJson(this);
+}
+
+Person q_$PersonFromJson(Map<String, dynamic> json) => Person(
+  firstName: json['firstName'] as String,
+  lastName: json['lastName'] as String,
+  dateOfBirth: json['dateOfBirth'] == null
+      ? null
+      : DateTime.parse(json['dateOfBirth'] as String),
+);
+
+Map<String, dynamic> _$PersonToJson(Person instance) => <String, dynamic>{
+  'firstName': instance.firstName,
+  'lastName': instance.lastName,
+  'dateOfBirth': instance.dateOfBirth?.toIso8601String(),
+};
+
 
 class ApiTask {
   int id;
